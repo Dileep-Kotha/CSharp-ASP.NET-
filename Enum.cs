4b22a0bc5192deaa120a86d17368df9e8d7f146b
Dileep-Kotha <kothadileep2002@gmail.com>
2025-05-11 00:32:25 +0530

    Add files via upload

---
+++ Enum.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enums
+{
+    enum days
+    {
+        monday,
+        tuesday,wednesday,thursday,friday,saturday,sunday
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            days today =days.friday;
+            Console.WriteLine(today);//default it is string
+            Console.WriteLine((int)today);//converted to integer value
+        }
+    }
+}

1bfe4728cf0184743ab114d7e5a24d7b27581692
Dileep-Kotha <kothadileep2002@gmail.com>
2025-05-10 23:52:40 +0530

    Add files via upload

---
+++ Delegates.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Delegates
+{
+    
+    internal class Program
+    {
+        public delegate int Del(int x,int y);
+        static void Main(string[] args)
+        {
+            char choice;
+            int x, y;
+            int res = 0;
+            string str;
+            Del d;
+            Console.WriteLine("##----CALCULATOR----##");
+            do
+            {
+                Console.WriteLine("enter first number:");
+                x = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("enter second number:");
+                y = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("enter the operation:");
+                choice = Convert.ToChar(Console.ReadLine());
+                switch (choice)
+                {
+                    case '+':
+                            d = new Del(Add);
+                            res = d(x, y);
+                            Console.WriteLine($"{x}+{y}={res}");
+                            break;
+                    case '*':
+                            d = new Del(mul);
+                            res = d(x, y);
+                            Console.WriteLine($"{x}*{y}={res}");
+                            break;
+                    case '/':
+                            d = new Del(div);
+                            res = d(x, y);
+                            Console.WriteLine($"{x}/{y}={res}");
+                            break;
+                    case '-':
+                            d = new Del(sub);
+                            res = d(x, y);
+                            Console.WriteLine($"{x}-{y}={res}");
+                            break;
+                    case '%':
+                            d = new Del(mod);
+                            res = d(x, y);
+                            Console.WriteLine($"{x}%{y}={res}");
+                            break;
+                    default:
+                            Console.WriteLine("enter correct option");
+                            break;
+                }
+                Console.WriteLine("enter y to continue:");
+                str= Console.ReadLine();
+            } while (str.ToLower() == "y");
+            
+        }
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+        public static int mul(int x, int y)
+        {
+            return x * y;
+        }
+        public static  int div(int x, int y)
+        {
+            return x / y;
+        }
+        public static int mod(int x, int y)
+        {
+            return x % y;
+        }
+        public static int sub(int x, int y)
+        {
+            return x - y;
+        }
+    }
+}
---
+++ MulticastDelegates.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompleteDelegates
+{
+    public delegate void Print(string name);
+    internal class Program
+    {
+        public static void display(string x)
+        {
+            Console.WriteLine("name in display is "+x);
+        }
+        public void show(string y)
+        {
+            Console.WriteLine("name in show is " + y);
+        }
+        static void Main(string[] args)
+        {
+            Program p=new Program();
+            Print del =new Print(p.show);//it should be called with object because it is non-static method
+            del += Program.display;//multicast Delegates
+            //called using class name because it is static method
+            del("dileep");
+        }
+    }
+}
